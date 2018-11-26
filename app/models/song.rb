70c562401161c4e8c5a14d0d014406d5b2211a1b
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-26 15:31:46 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,12 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :title, uniqueness: { scope: [:release_year, :artist_name] }
+  with_options if: :released do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { only_integer: true }
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+songs = Song.create([
+  {title: "song1", released: true, release_year: 1990, artist_name: "artist1", genre: "genre1"},
+  {title: "song2", released: false, release_year: 1991, artist_name: "artist2", genre: "genre2"},
+  {title: "song3", released: true, release_year: 1994, artist_name: "artist3", genre: "genre3"},
+  {title: "song4", released: false, release_year: 1995, artist_name: "artist4", genre: "genre4"},
+  {title: "song5", released: true, release_year: 1990, artist_name: "artist5", genre: "genre5"},
+  {title: "song6", released: false, release_year: 1997, artist_name: "artist6", genre: "genre6"},
+  {title: "song7", released: true, release_year: 1990, artist_name: "artist7", genre: "genre7"},
+  {title: "song8", released: false, release_year: 1999, artist_name: "artist8", genre: "genre8"},
+  {title: "song9", released: true, release_year: 2000, artist_name: "artist9", genre: "genre9"}
+  ])
